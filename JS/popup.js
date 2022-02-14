fdd582f3d089aba70cc347d6c0e8aa7d3070075d
Imen1028 <rehabimen88@gmail.com>
2022-02-14 15:04:18 +0800

    Mostly done!!

---
+++ JS/popup.js
@@ -0,0 +1,11 @@
+console.log("Hey there!")
+
+function showPopup () {
+    var popup = document.getElementById('popup');
+    popup.classList.add('show')
+}
+
+function closePopup () {
+    var popup = document.getElementById('popup');
+    popup.classList.remove('show')
+}
